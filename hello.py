c2b1e8e7d59d13a2728c3e6b7300b668a5349972
Maroua El Amarti <marouaelmarti@gmail.com>
2026-05-19 11:56:28 +0100

    premier script jenkins

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("=" * 40)
+print("bienvenue dans mon premier job jenkins!"
+print("=" * 40)
+nom=input("quel est ton nom?")
+print(f"bonjour {nom},ton job jenkins a reussi!")
+a=10
+b=5
+print(f"{a}+{b}={a+b}")
+print(f"{a}-{b}={a-b}")
